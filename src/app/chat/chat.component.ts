861561cdd3bf22dc3292aa8fa1b9d10e9d7cce3b
jamesk111 <8887227+jamesk111@users.noreply.github.com>
2019-08-23 23:39:55 -0600

    Add username to chat emits // Code formatting

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '@src/app/chat.service';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '@src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -10,21 +12,24 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 export class ChatComponent implements OnInit {
 
   msg: string;
-  messages: string[] = [];
+  messages: object[] = [];
   faPaperPlane = faPaperPlane;
+  username: Subscription = this.auth
+      .getUser$()
+      .subscribe(user => this.username = user.nickname);
 
-  constructor(private chatService: ChatService) {
+  constructor(private chatService: ChatService, private auth: AuthService) {
   }
 
   sendMessage() {
-    this.chatService.sendMessage(this.msg);
+    this.chatService.sendMessage({name: this.username, message: this.msg});
     this.msg = '';
   }
 
   ngOnInit() {
     this.chatService
         .getMessages()
-        .subscribe((msg: string) => {
+        .subscribe((msg: object) => {
           this.messages.push(msg);
           console.log(msg);
         });
